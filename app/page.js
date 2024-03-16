ec75809e95e34d92f81fcc39e90537d3928139b4
tarafdar16 <imrantarafdar16@gmail.com>
2024-03-16 15:28:00 +0600

    project done

---
+++ app/page.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import Header from "./components/Header";
+import TopCards from "./components/TopCards";
+import BarChart from "./components/BarChart";
+import RecentOrders from "./components/RecentOrders";
+
+export default function Home() {
+  return (
+    <main className="bg-gray-100 min-h-screen">
+      <Header />
+      <TopCards />
+      <div className="grid md:grid-cols-3 grid-cols-1 p-4 gap-4">
+        <BarChart/>
+        <RecentOrders/>
+      </div>
+    </main>
+  );
+}
